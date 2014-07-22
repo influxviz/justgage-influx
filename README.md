5e602d1eaad13ea4b1a32108d22bfc69bde77e78
ArminTaheri <armin.ytaheri@gmail.com>
2014-07-22 18:25:22 -0400

    Added the ability to place gauges using html alone.

--- README.md
+++ README.md
@@ -3,38 +3,21 @@ justgage-influx
 
 How to:
 =======
-The graphHandler.js script contains an interface to creating the gauges.
+The graphHandler.js script contains an interface to creating and placing gauges in your html page.
 
-First you'll want to enter the credentials of your database where you see:
-```
-var Hostname = "http://162.243.57.240:8086"; //Make sure no forward slash procedes the hostname.
-var Database = "home-sensor-array";
-var Username = "root";
-var PassWord = "root";
-```
-Create a div in your html page as follows at minimum:
-```
-<div id="gauge-id" class="200x160px"></div>
-```
-Create a new object inside the gauges object as follows:
+First you'll want to enter the credentials inside a metatag like so:
 
 ```
-var gauges = {
-  exampleGauge: { //This is how your new object begins
-    gauge: new JustGage({
-      id: "gauge-id",
-      min: 0,
-      max: 100,
-      value: 0,
-      title: "title of gauge"
-      //...etc (visit http://www.justgage.com for more formatting options)
-    }),
-    
-    query: "select entryName from timeSeries"
-  }, //This is where it ends.
+<meta name="influxDBCredentials" content="databaseIP: 162.243.57.240; port: 8086; database: home-sensor-array; username: root; password: root;">
 ```
-The property names that you cannot change are the gauge property and the query propterty
-for they are used to actually update the gauge itself and send the query to the database
-respectively.
 
-The rest is handled by jQuery and your div's css properties.
+Create a div in your html page with a meta-tag inside it containing the gauges configuration as follows:
+```
+<div id="gauge-id" class="gauge">
+  <meta content="influxValue: value; influxTimeSeries: sensor; min: 0; max: 100; title: awesome gauge example;">
+</div>
+```
+Note: the influxValue and influxTimeSeries values are used to form the query "select influxValue from influxTimeSeries".
+
+
+The rest is handled by influxDBGauge.js, jQuery.js and your div's css properties.
---
+++ influxDBGauge.js
@@ -0,0 +1,185 @@
+/*	
+	This script is used to populate the html files that
+	use it with JustGage gauges. The gauges are updated
+	using values from an influxDB database.
+*/
+
+var influxGauge = function (config) {
+	var query = "";
+
+	query += "select ";
+	query += config.influxValue.split(" ")[0] + " ";
+	query += "from ";
+	query += config.influxTimeSeries.split(" ")[0];
+
+
+
+	this.gauge = new JustGage(config);
+	this.query = query;
+};
+
+var parseMetaProperties = function(meta) {
+	var metaContents = meta.getAttribute("content");
+
+	metaContents = metaContents.split(";");
+
+	var properties = {};
+	
+	for(var i = 0; i < metaContents.length; i++)
+	{
+		if(metaContents[i].trim() == "")
+		{
+			continue;
+		}
+		
+		var keyValuePair = metaContents[i].split(":");
+		var key = keyValuePair[0].trim();
+		var value = keyValuePair[1].trim();
+
+		if(key && value)
+		{
+			properties[key] = value;
+		}
+	}
+
+	return properties;
+};
+
+var getDatabaseCredentials = function() {
+	var metas = document.getElementsByName("influxDBCredentials");
+	var meta = metas[metas.length - 1];
+	var credentials = parseMetaProperties(meta);
+
+	if(credentials.port == undefined)
+	{
+		credentials.port = "8086";
+	}
+
+	return credentials;
+};
+
+var createGaugeObjects = function () {
+	var gauges = $(".gauge").toArray();
+	var influxGauges = [];
+
+	for(var i = 0; i < gauges.length; i++)
+	{
+		if(gauges[i].nodeName != "DIV")
+		{
+			return influxGauges;
+		}
+
+		var metas = gauges[i].getElementsByTagName("meta");
+		var meta = metas[metas.length - 1];
+		
+		if(meta == undefined)
+		{ 
+			return influxGauges;
+		}
+
+		if(gauges[i].id == undefined)
+		{
+			return influxGauges;
+		}
+
+		var gaugeProperties = parseMetaProperties(meta);
+		gaugeProperties["id"] = gauges[i].id;
+		gaugeProperties["labelFontColor"] = "#000000"; 
+		gaugeProperties["titleFontColor"] = "#000000"
+		gaugeProperties["relativeGaugeSize"] = true;
+
+		influxGauges.push(new influxGauge(gaugeProperties));
+
+	}
+
+	return influxGauges;
+};
+
+//GET request with the query will be sent to the influxDB database.
+var influxDBUpdateGauge = function(credentials, query, gauge, callback) {
+	var url = "http://" + credentials.databaseIP + ":" + credentials.port + "/db/" + credentials.database + "/series?u=" 
+		+ credentials.username + "&p=" + credentials.password + "&q=" + query + "&" 
+		+ Math.floor(Math.random()*11240912); //Keep from hitting browser cache.
+
+	$.get(
+		url, 
+		function(res){
+		
+			var data = jQuery.parseJSON(res.substring(1,res.length - 1));
+
+			var points = data["points"][0]
+			callback(Math.floor(points[2]), gauge);
+		},
+		"text")
+};
+
+//This function allows the JustGage.refresh function to be used as a callback.
+var refreshGauge = function(val, gauge) {
+	gauge.refresh(val);
+}
+
+//This function will force all the gauges to sync with the latest value outputed by the database.
+var sendQueries = function(gauges, credentials) {
+	for (var i = 0; i < gauges.length; i++)
+	{
+		var gauge = gauges[i]["gauge"];
+		var query = gauges[i]["query"];
+		if (gauge instanceof JustGage)
+		{
+			influxDBUpdateGauge(credentials, query, gauge, refreshGauge);
+		}
+	}
+};
+
+var checkSidebarRetract = function() {
+	if($(window).width() < 700)
+	{
+		$("#wrapper").css({"padding-left":"0"});
+	}
+	else
+	{
+		$("#wrapper").css({"padding-left":"250px"});
+	}
+};
+
+
+
+$(document).ready(function(){
+	var gauges = createGaugeObjects();
+	var credentials = getDatabaseCredentials();
+
+	checkSidebarRetract();
+	sendQueries(gauges, credentials);
+
+	$(window).resize(function(){
+		checkSidebarRetract();
+	});
+
+	
+	var queryInterval = setInterval(function() {sendQueries(gauges, credentials);}, 1500);
+});
+
+
+
+
+
+
+
+
+
+/*
+TODO: second page with a graph and a widget handler.
+
+var widgets = ["#indoor-stats", "#garden-stats"];
+
+var activeWidget = widgets[0];
+
+var swapWidgets = function(widgetNumber) {
+	var num = (widgetNumber > widgets.length - 1) ? widgets.length - 1 : widgetNumber;
+
+	$(activeWidget).slideUp("slow",function(){
+		activeWidget = widgets[widgetNumber]
+		$(activeWidget).slideDown();
+	});
+}
+*/
